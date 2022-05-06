b204ca0ce50af356915af4becfbadf6f58536fcb
mmlanas <matias.m.lanas@gmail.com>
2022-05-06 13:49:48 -0300

    Create project

---
+++ BiblioSpring/src/main/java/bibliospring/BiblioSpringApplication.java
@@ -0,0 +1,13 @@
+package bibliospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioSpringApplication.class, args);
+	}
+
+}
---
+++ BiblioSpring/src/test/java/bibliospring/BiblioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package bibliospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
