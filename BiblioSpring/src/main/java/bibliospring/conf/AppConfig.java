bab08c26d6b073212bdbfcfc64f052537225e292
mmlanas <matias.m.lanas@gmail.com>
2022-05-06 23:49:12 -0300

    create data base

---
+++ BiblioSpring/src/main/java/bibliospring/conf/AppConfig.java
@@ -0,0 +1,10 @@
+package bibliospring.conf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("bibliospring.*")
+public class AppConfig {
+
+}
